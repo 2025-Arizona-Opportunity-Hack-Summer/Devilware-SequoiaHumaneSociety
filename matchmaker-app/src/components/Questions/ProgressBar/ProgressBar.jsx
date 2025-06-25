1e8df482b09e34283e1b45d5f8be2ae3102c21dc
KhoaDinhNguyen <khoacode1305@gmail.com>
2025-06-24 20:16:20 -0700

    feat: create redux for storing answer

--- matchmaker-app/src/components/Questions/ProgressBar/ProgressBar.jsx
+++ matchmaker-app/src/components/Questions/ProgressBar/ProgressBar.jsx
@@ -7,19 +7,19 @@ function ProgressBar({ currIdx }) {
 
   switch (currIdx) {
     case 0:
-      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[10%]";
+      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[10%] transition-all duration-700";
       break;
     case 1:
-      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[30%]";
+      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[30%] transition-all duration-700";
       break;
     case 2:
-      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[50%]";
+      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[50%] transition-all duration-700";
       break;
     case 3:
-      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[70%]";
+      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[70%] transition-all duration-700";
       break;
     case 4:
-      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[100%]";
+      widthBar = "relative h-1 bg-[#7C0F0F] flex mb-20 w-[100%] transition-all duration-700";
       break;
   }
 
@@ -28,7 +28,10 @@ function ProgressBar({ currIdx }) {
     <div className="relative h-1 bg-[#EFEFEF] flex mb-20 xl:w-[1000px]">
       <div className={widthBar}></div>
       <div className="absolute translate-[-50%] top-[500%] left-[10%] flex flex-col items-center max-w-[100px]">
-        <div className={`w-12 h-12 ${currIdx < 0 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"} rounded-full`}></div>
+        <div
+          className={`w-12 h-12 ${
+            currIdx < 0 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"
+          } rounded-full transition-all duration-700`}></div>
         <div>
           <p className={`text-center ${currIdx < 0 ? "text-[#ced4da]" : "text-[#7C0F0F]"} font-semibold`}>
             Housing Environment
@@ -36,7 +39,10 @@ function ProgressBar({ currIdx }) {
         </div>
       </div>
       <div className="absolute translate-[-50%] top-[500%] left-[30%] flex flex-col items-center max-w-[100px]">
-        <div className={`w-12 h-12 ${currIdx < 1 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"} rounded-full`}></div>
+        <div
+          className={`w-12 h-12 ${
+            currIdx < 1 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"
+          } rounded-full transition-all duration-700`}></div>
         <div>
           <p className={`text-center ${currIdx < 1 ? "text-[#adb5bd]" : "text-[#7C0F0F]"} font-semibold`}>
             Household Composition
@@ -44,7 +50,10 @@ function ProgressBar({ currIdx }) {
         </div>
       </div>
       <div className="absolute translate-[-50%] top-[500%] left-[50%] flex flex-col items-center max-w-[100px]">
-        <div className={`w-12 h-12 ${currIdx < 2 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"} rounded-full`}></div>
+        <div
+          className={`w-12 h-12 ${
+            currIdx < 2 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"
+          } rounded-full transition-all duration-700`}></div>
         <div>
           <p className={`text-center ${currIdx < 2 ? "text-[#adb5bd]" : "text-[#7C0F0F]"} font-semibold`}>
             Lifestyle & Commitment
@@ -52,7 +61,10 @@ function ProgressBar({ currIdx }) {
         </div>
       </div>
       <div className="absolute translate-[-50%] top-[500%] left-[70%] flex flex-col items-center max-w-[100px]">
-        <div className={`w-12 h-12 ${currIdx < 3 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"} rounded-full`}></div>
+        <div
+          className={`w-12 h-12 ${
+            currIdx < 3 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"
+          } rounded-full transition-all duration-700`}></div>
         <div>
           <p className={`text-center ${currIdx < 3 ? "text-[#adb5bd]" : "text-[#7C0F0F]"} font-semibold`}>
             Experience & Expectations
@@ -60,7 +72,10 @@ function ProgressBar({ currIdx }) {
         </div>
       </div>
       <div className="absolute translate-[-50%] top-[500%] left-[90%] flex flex-col items-center max-w-[100px]">
-        <div className={`w-12 h-12 ${currIdx < 4 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"} rounded-full`}></div>
+        <div
+          className={`w-12 h-12 ${
+            currIdx < 4 ? "bg-[#EFEFEF]" : "bg-[#7C0F0F]"
+          } rounded-full transition-all duration-700`}></div>
         <div>
           <p className={`text-center ${currIdx < 4 ? "text-[#adb5bd]" : "text-[#7C0F0F]"} font-semibold`}>
             Specific Preferences
@@ -69,7 +84,6 @@ function ProgressBar({ currIdx }) {
       </div>
     </div>
   );
-  return <div className={styles[percentage]}></div>;
 }
 
 export default ProgressBar;
--- matchmaker-app/src/redux/HousingEnvironmentSlice.jsx
+++ matchmaker-app/src/redux/HousingEnvironmentSlice.jsx
@@ -30,10 +30,32 @@ export const he3Slice = createSlice({
   },
 });
 
+export const he4Slice = createSlice({
+  name: "he4",
+  initialState: "",
+  reducers: {
+    assign(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const he5Slice = createSlice({
+  name: "he5",
+  initialState: "",
+  reducers: {
+    assign(state, action) {
+      return action.payload;
+    },
+  },
+});
+
 const HEQuestionsSlice = {
   [he1Slice.name]: he1Slice.reducer,
   [he2Slice.name]: he2Slice.reducer,
   [he3Slice.name]: he3Slice.reducer,
+  [he4Slice.name]: he4Slice.reducer,
+  [he5Slice.name]: he5Slice.reducer,
 };
 
 export default HEQuestionsSlice;

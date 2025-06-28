b94d9a34a7d79b39d60f09dbc697a67091a5a594
GittBitt <nphuynh2@asu.edu>
2025-06-28 02:13:44 -0500

    Removed PageWrapper, add vectors
    - Removed unnecessary PageWrapper component
    - Added vectors for decor

--- matchmaker-app/src/components/PageWrapper/PageWrapper.jsx
+++
@@ -1,8 +0,0 @@
-function PageWrapper({ children, className = "" }) {
-  if (className === "match") {
-    return <div className={`pt-0 match`}>{children}</div>;
-  }
-  return <div className={`pt-20 md:pt-24 ${className}`}>{children}</div>;
-}
-
-export default PageWrapper;
--- matchmaker-app/src/pages/Register/Register.jsx
+++ matchmaker-app/src/pages/Register/Register.jsx
@@ -1,10 +1,8 @@
-import PageWrapper from "../../components/PageWrapper/PageWrapper";
-
 function Register() {
   return (
-    <PageWrapper className="register">
+    <div className="register">
       <p>This is register</p>
-    </PageWrapper>
+    </div>
   );
 }
 

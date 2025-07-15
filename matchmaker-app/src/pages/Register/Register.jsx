7a82d0267a646ef670c4b0ec2b6763d208c3a392
Ngoc Dinh Khoa Nguyen <khoacode1305@gmail.com>
2025-07-14 21:48:27 -0700

    feat: create sign in modal

--- matchmaker-app/src/pages/Register/Register.jsx
+++ matchmaker-app/src/pages/Register/Register.jsx
@@ -485,7 +485,7 @@ function Register() {
       <Modal visible={visibleModal} className="!w-max !h-max !right-0 !top-1/8 !bg-transparent ">
         <div className="bg-white border-2 border-l-0 border-[#a4133c] max-w-[250px] border-r-4 py-2 px-5 error-modal">
           <div className="flex gap-2 items-center">
-            <CannotSignUp />
+            <WarningSVG />
             <p className="font-semibold uppercase text-[#a4133c]">Cannot sign up</p>
           </div>
           <p>{getError}</p>
@@ -495,7 +495,7 @@ function Register() {
   );
 }
 
-function CannotSignUp() {
+function WarningSVG() {
   return (
     <svg className="w-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path

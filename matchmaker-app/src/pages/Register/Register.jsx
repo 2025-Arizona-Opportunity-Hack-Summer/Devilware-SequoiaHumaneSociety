9024e0e11b9a163c5beb15e4ce1eb71282d3afcc
Ngoc Dinh Khoa Nguyen <khoacode1305@gmail.com>
2025-07-17 22:03:01 -0700

    update: update session storage in match page

--- matchmaker-app/src/pages/Register/Register.jsx
+++ matchmaker-app/src/pages/Register/Register.jsx
@@ -187,7 +187,7 @@ function Register() {
             />
             <label
               htmlFor="sign-up-google-button"
-              className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors mb-4">
+              className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors mb-4 cursor-pointer">
               <img src={googleLogo} alt="Google" className="w-5 h-5 mr-2" />
               Register with Google
             </label>
